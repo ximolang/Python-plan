11a46a06037fd231d04c2fa96f162bf71f99cba2
ximolang <ximolang@yeah.net>
2018-03-11 23:13:25 +0800

    ..

---
+++ liaoxuefeng/exc-function.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+# 空函数
+def nop():
+    pass
+
+# 返回多个值
+def returnVals():
+    return 1,2,3
+
+# 默认参数
+def power(x, n=2):
+    s = 1
+    while n > 0:
+        s = s * x
+        n = n - 1
+    return s
+
+results = returnVals()
+print(results[0])
+
+print(power(10, 3))
+print(power(10))
